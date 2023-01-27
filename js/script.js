1786a6630686b2ee524c16b6a752079044248203
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-01-27 12:09:15 -0500

    about section opens (but doesn't close)

--- js/script.js
+++ js/script.js
@@ -6,6 +6,16 @@ $(".pages-cluster-title").click(function(){
     $(this).parent().find(".project-lists").toggleClass("menuSectionOpened")
 })
 
+// Open about section when about button is open
+
+$("#about-button").click(function(){
+    // $("#about-section").toggleClass("about-section-opened")
+    $("header").toggleClass("header-opened");
+    $(".header-img").toggleClass("header-img-opened")
+})
+
+
+
 // Mail to sarah.hm@hotmail.ca when contact is clicked
 $("#contact-button").click(function(){
     window.location.href=`mailto:sarah.hm@hotmail.ca`
