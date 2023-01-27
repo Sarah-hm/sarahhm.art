0381b5f14a3184d7d563ac3862c5d111a972359c
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-01-27 11:53:22 -0500

    functional contact mailto button and link

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
 
 
-
+// Hamburger menu for projects section
 $(".pages-cluster-title").click(function(){
     $(this).parent().find(".project-lists").toggleClass("menuSectionOpened")
 })
 
-
+// Mail to sarah.hm@hotmail.ca when contact is clicked
+$("#contact-button").click(function(){
+    window.location.href=`mailto:sarah.hm@hotmail.ca`
+})
 
 })
