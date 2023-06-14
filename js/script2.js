e47ecfe42082b65201ae1ff8338fc34926a86750
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-06-13 22:31:50 -0400

    some dark mode effect

--- js/script2.js
+++ js/script2.js
@@ -1,11 +1,38 @@
 window.onload = (event) => {
+  // DOM ELEMENTS variables
+  const page = document.getElementById("page");
   const darkModeBtnContainer = document.getElementById("dark-mode-toggle-btn");
-  const CheckBoxes = darkModeBtnContainer.getElementsByTagName("input");
-  const darkModeCheckBox = CheckBoxes[0];
+  const darkModeCheckbox = darkModeBtnContainer.querySelector(
+    "input[name=dark-mode-checkbox]"
+  );
+  let hoveringTitles = document.getElementsByClassName("hovering-titles");
 
-  if (darkModeCheckBox.checked) {
-    alert("checked");
-  } else {
-    alert("You didn't check it! Let me check it for you.");
-  }
+  //colors
+  const black = "rgb(0,0,0)";
+  const white = "rgb(255,255,255)";
+
+  darkModeCheckbox.addEventListener("change", function () {
+    if (this.checked) {
+      console.log(hoveringTitles.length);
+      //DARK MODE IS TURNED ON
+      console.log("Checkbox is checked..");
+      page.style.backgroundColor = black;
+
+      //change hovering titles to dark mode
+      for (let i = 0; i < hoveringTitles.length; i++) {
+        hoveringTitles[i].classList.remove("light-mode");
+        hoveringTitles[i].classList.add("dark-mode");
+      }
+    } else {
+      console.log("dark mode");
+      //LIGHT MODE IS TURNED ON
+      page.style.backgroundColor = white;
+
+      //change hovering titles to light mode
+      for (let i = 0; i < hoveringTitles.length; i++) {
+        hoveringTitles[i].classList.remove("light-mode");
+        hoveringTitles[i].classList.add("dark-mode");
+      }
+    }
+  });
 };
