bfa6c295f13bf524f2bf01ba28d2ca764aeaaafa
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-06-20 19:41:52 -0400

    dark mode actually works

--- js/script2.js
+++ js/script2.js
@@ -6,7 +6,7 @@ window.onload = (event) => {
     "input[name=dark-mode-checkbox]"
   );
   let hoveringTitles = document.getElementsByClassName("hovering-titles");
-
+  let inTitle = document.getElementById("title-in");
   //colors
   const black = "rgb(0,0,0)";
   const white = "rgb(255,255,255)";
@@ -15,23 +15,24 @@ window.onload = (event) => {
     if (this.checked) {
       console.log(hoveringTitles.length);
       //DARK MODE IS TURNED ON
-      console.log("Checkbox is checked..");
+      console.log("dark mode");
       page.style.backgroundColor = black;
-
+      inTitle.style.color = white;
       //change hovering titles to dark mode
       for (let i = 0; i < hoveringTitles.length; i++) {
         hoveringTitles[i].classList.remove("light-mode");
         hoveringTitles[i].classList.add("dark-mode");
       }
     } else {
-      console.log("dark mode");
+      console.log("light mode");
       //LIGHT MODE IS TURNED ON
       page.style.backgroundColor = white;
+      inTitle.style.color = black;
 
       //change hovering titles to light mode
       for (let i = 0; i < hoveringTitles.length; i++) {
-        hoveringTitles[i].classList.remove("light-mode");
-        hoveringTitles[i].classList.add("dark-mode");
+        hoveringTitles[i].classList.remove("dark-mode");
+        hoveringTitles[i].classList.add("light-mode");
       }
     }
   });
