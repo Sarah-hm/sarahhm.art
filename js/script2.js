530e215f717e11e0da61333390525691c94e9a42
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-06-23 16:43:48 -0400

    change menu hover animation, creating submenu

--- js/script2.js
+++ js/script2.js
@@ -5,8 +5,10 @@ window.onload = (event) => {
   const darkModeCheckbox = darkModeBtnContainer.querySelector(
     "input[name=dark-mode-checkbox]"
   );
+  let titles = document.getElementsByClassName("titles");
   let hoveringTitles = document.getElementsByClassName("hovering-titles");
-  let inTitle = document.getElementById("title-in");
+
+  let subsections = document.getElementsByClassName("subsection");
   //colors
   const black = "rgb(0,0,0)";
   const white = "rgb(255,255,255)";
@@ -17,23 +19,40 @@ window.onload = (event) => {
       //DARK MODE IS TURNED ON
       console.log("dark mode");
       page.style.backgroundColor = black;
-      inTitle.style.color = white;
-      //change hovering titles to dark mode
-      for (let i = 0; i < hoveringTitles.length; i++) {
-        hoveringTitles[i].classList.remove("light-mode");
-        hoveringTitles[i].classList.add("dark-mode");
+
+      //change  titles to dark mode
+      for (let i = 0; i < titles.length; i++) {
+        titles[i].classList.remove("light-mode");
+        titles[i].classList.add("dark-mode");
+      }
+
+      for (let i = 0; i < subsections.length; i++) {
+        //  Make subsection follow dark mode
       }
     } else {
       console.log("light mode");
       //LIGHT MODE IS TURNED ON
       page.style.backgroundColor = white;
-      inTitle.style.color = black;
 
-      //change hovering titles to light mode
-      for (let i = 0; i < hoveringTitles.length; i++) {
-        hoveringTitles[i].classList.remove("dark-mode");
-        hoveringTitles[i].classList.add("light-mode");
+      //change titles to light mode
+      for (let i = 0; i < titles.length; i++) {
+        titles[i].classList.remove("dark-mode");
+        titles[i].classList.add("light-mode");
       }
     }
   });
+
+  // Doesn't work cause it's applied to the title and the its related subsection
+  for (let i = 0; i < hoveringTitles.length; i++) {
+    hoveringTitles[i].addEventListener("click", function () {
+      if (!this.classList.contains("subsection-opened")) {
+        this.classList.add("subsection-opened");
+      } else {
+        this.classList.remove("subsection-opened");
+      }
+
+      // If it is not opened, put the class opened on it + make sure all the other ones are closed (remove class for all others?)
+      // If it has the class opened, close it.
+    });
+  }
 };
