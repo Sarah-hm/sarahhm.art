51af7cab2a1498069549a081c3ec6d5b54b066ec
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2023-08-10 07:54:27 -0400

    retrieve all portfolio JSON attributes in html/js

--- js/project.js
+++ js/project.js
@@ -1,36 +1,152 @@
 class Project {
-  constructor(doc, title, subtitle, year) {
+  constructor(
+    title,
+    subtitle,
+    year,
+    media,
+    keywords,
+    websiteLink,
+    desc,
+    visualDoc,
+    linkDoc
+  ) {
     this.title = title;
     this.subtitle = subtitle;
     this.year = year;
+    this.media = media;
+    this.keywords = keywords;
+    this.websiteLink = websiteLink;
+    this.desc = desc;
+    this.visualDoc = visualDoc;
+    this.linkDoc = linkDoc;
     // this.media[("one", "two", "three")];
 
-    this.doc = doc;
+    this.doc = document.body;
 
     this.printTitle();
     this.printSubtitle();
     this.printYear();
+    this.printMedia();
+    this.printKeywords();
+    this.printWebsiteLink();
+    this.printDescription();
+    this.printVisualDoc();
+    this.printLinkDoc();
   }
 
   printTitle() {
     this.para = document.createElement("p");
     this.para.classList.add("prj-title");
     this.doc.appendChild(this.para);
-
-    this.doc.querySelector(".prj-title").innerHTML = this.title;
+    this.para.insertAdjacentText("afterbegin", this.title);
   }
 
   printSubtitle() {
     this.para = document.createElement("p");
     this.para.classList.add("prj-subtitle");
     this.doc.appendChild(this.para);
-    this.doc.querySelector(".prj-subtitle").innerHTML = this.subtitle;
+    this.para.insertAdjacentText("afterbegin", this.subtitle);
   }
 
   printYear() {
     this.para = document.createElement("p");
     this.para.classList.add("prj-year");
     this.doc.appendChild(this.para);
+    this.para.insertAdjacentText("afterbegin", this.year);
+  }
+
+  printMedia() {
+    // open a div that stores all media with an ul
+    this.ul = document.createElement("ul");
+    this.ul.classList.add("prj-media-ul");
+    this.doc.appendChild(this.ul);
+
+    for (this.i = 0; this.i < this.media.length; this.i++) {
+      //print single medium as a li
+      this.li = document.createElement("li");
+      this.li.classList.add("prj-media-li");
+      this.ul.appendChild(this.li);
+
+      this.medium = document.createTextNode(`${this.media[this.i]}`);
+
+      this.li.appendChild(this.medium);
+    }
+  }
+
+  printKeywords() {
+    // open a div that stores all keywords with an ul
+    this.ul = document.createElement("ul");
+    this.ul.classList.add("prj-keywords-ul");
+    this.doc.appendChild(this.ul);
+
+    for (this.i = 0; this.i < this.keywords.length; this.i++) {
+      //print single medium as a li
+      this.li = document.createElement("li");
+      this.li.classList.add("prj-keywords-li");
+      this.ul.appendChild(this.li);
+      this.keyword = document.createTextNode(`${this.keywords[this.i]}`);
+      this.li.appendChild(this.keyword);
+    }
+  }
+
+  //create button for links to the project-website
+  printWebsiteLink() {
+    this.div = document.createElement("div");
+    this.div.classList.add("prj-websiteLink-btn");
+    this.doc.appendChild(this.div);
+
+    this.link = `<a href = "${this.websiteLink}">${this.websiteLink}</a>`;
+    this.div.insertAdjacentHTML("afterbegin", this.link);
+
     this.doc.querySelector(".prj-year").innerHTML = `${this.year}`;
+  }
+
+  printDescription() {
+    // open a div that stores all description with an ul
+    this.div = document.createElement("div");
+    this.div.classList.add("prj-desc-div");
+    this.doc.appendChild(this.div);
+
+    for (this.i = 0; this.i < this.desc.length; this.i++) {
+      //print single medium as a li
+      this.para = document.createElement("para");
+      this.para.classList.add("prj-desc-para");
+      this.div.appendChild(this.para);
+      this.descPara = document.createTextNode(`${this.desc[this.i]}`);
+      this.para.appendChild(this.descPara);
+    }
+  }
+
+  //Add images to slideshow (or video*)
+  printVisualDoc() {
+    // open a div that stores all keywords with an ul
+    this.div = document.createElement("div");
+    this.div.classList.add("prj-visualDoc-div");
+    this.doc.appendChild(this.div);
+
+    for (this.i = 0; this.i < this.visualDoc.length; this.i++) {
+      //print single medium as a li
+      this.img = document.createElement("img");
+      this.img.src = `${this.visualDoc[this.i]}`;
+      this.img.classList.add("prj-visualDoc-img");
+      this.div.appendChild(this.img);
+    }
+  }
+
+  //create buttons for extra external documentation
+  printLinkDoc() {
+    this.div = document.createElement("div");
+    this.div.classList.add("prj-link-documentation-div");
+    this.doc.appendChild(this.div);
+
+    for (this.i = 0; this.i < this.linkDoc.length; this.i++) {
+      this.div = document.createElement("div");
+      this.div.classList.add("prj-visual-documentation-btn");
+      this.doc.appendChild(this.div);
+      this.link = `<a href = "${this.linkDoc[this.i]}">${
+        this.linkDoc[this.i]
+      }</a>`;
+      this.div.insertAdjacentHTML("afterbegin", this.link);
+    }
   }
 }
--- js/script-prj.js
+++ js/script-prj.js
@@ -5,16 +5,23 @@ window.onload = (event) => {
     .then((data) => {
       //push all the data in their own object
 
+      //make if statement to know if the document has a project (gas a meta data called project)
+
       // Depending on the ID of the project, send the new project object and its data to the class
       let projectID = document.body.getAttribute("project");
       //    Send all projects to a new project class, and in the class populate a
       for (let i = 0; i < data.length; i++) {
         if (data[i].id == projectID) {
           new Project(
-            document.body,
             data[i].title,
             data[i].subtitle,
-            data[i].year
+            data[i].year,
+            data[i].media,
+            data[i].keywords,
+            data[i].websiteLink,
+            data[i].description,
+            data[i].visual_documentation,
+            data[i].link_documentation
           );
         }
       }
