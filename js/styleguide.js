0a50dd44c6118ef534fc81764767de4e16da15f4
Sarah Hontoy-Major <sarah.hm@hotmail.ca>
2024-03-01 22:32:38 -0500

    working scrollbar

--- js/styleguide.js
+++ js/styleguide.js
@@ -1,6 +1,30 @@
 window.onload = () => {
-  //   window.addEventListener("scroll", (event) => {
-  //     let scroll = this.scrollY;
-  //     console.log(scroll);
-  //   });
+  const minScrollbarPos = (window.innerHeight / 10) * 9;
+  let maxScrollbarPos = window.innerHeight;
+  let fullDOMHeight = document.body.scrollHeight;
+
+  let scrollbar = document.getElementById("scrollbar");
+  scrollbar.style.bottom = `${minScrollbarPos}px`;
+  console.log(minScrollbarPos);
+  window.addEventListener("scroll", (event) => {
+    let currentScrollHeight = this.scrollY;
+
+    if (currentScrollHeight >= minScrollbarPos) {
+      console.log(currentScrollHeight, minScrollbarPos);
+      updateScrollbar(
+        currentScrollHeight,
+        fullDOMHeight,
+        minScrollbarPos,
+        maxScrollbarPos
+      );
+    }
+  });
+
+  function updateScrollbar(currentScrollHeight) {
+    let delta = 1 - currentScrollHeight / fullDOMHeight;
+
+    let newScrollbarBottomPos = delta * maxScrollbarPos;
+
+    scrollbar.style.bottom = `${newScrollbarBottomPos}px`;
+  }
 };
